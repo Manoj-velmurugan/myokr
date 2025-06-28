1975651d6842cbedad03190ad9fde77cb6e261b3
Manoj-velmurugan <manojvelmurugan04@gmail.com>
2025-06-28 17:12:31 +0530

    fix: point frontend to deployed backend

--- myokr-client/src/pages/Signup.jsx
+++ myokr-client/src/pages/Signup.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
-import axios from 'axios';
+import axios from '../utils/axiosInstance';
 import { jwtDecode } from 'jwt-decode';
 
 function Signup() {
