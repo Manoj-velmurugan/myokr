84642183861417972b2007bf1bbb087b59007a2c
Manoj-velmurugan <manojvelmurugan04@gmail.com>
2025-06-28 15:27:57 +0530

    fix: point frontend to deployed backend

--- myokr-client/src/utils/axiosInstance.js
+++ myokr-client/src/utils/axiosInstance.js
@@ -1,7 +1,8 @@
+// src/utils/axiosInstance.js
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api',
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'https://myokr-backend-98gs.onrender.com/api',
 });
 
 instance.interceptors.request.use((config) => {
