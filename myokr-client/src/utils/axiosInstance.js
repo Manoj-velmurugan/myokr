131105aea07b7135f7c6723e1e2805351c94676f
Manoj-velmurugan <manojvelmurugan04@gmail.com>
2025-06-28 16:08:53 +0530

    fix: point frontend to deployed backend

--- myokr-client/src/utils/axiosInstance.js
+++ myokr-client/src/utils/axiosInstance.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL, // must end with `/api`
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
+  withCredentials: true, // if you're using cookies
 });
 
 export default instance;
